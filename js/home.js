6d95a807e624f167a5f0e1b8ddfd01b82bd3859d
jeffyactive <jeff@reelyactive.com>
2025-10-13 10:01:08 -0400

    Added links and improved look of QA table

--- js/home.js
+++ js/home.js
@@ -1,5 +1,7 @@
 // Constants
 const QA_ELEMENTS = [ '#Q1', '#A1', '#Q2', '#A2', '#Q3', '#A3', '#Q4', '#A4' ];
+const QA_ROWS = [ '#R1', '#R2', '#R3', '#R4' ];
+const QA_LINKS = [ '#L1', '#L2', '#L3', '#L4' ];
 const QA_UPDATE_MILLISECONDS = 1600;
 
 
@@ -17,10 +19,20 @@ function updateQA() {
           QA_ELEMENTS[(qaIndex - 2 + QA_ELEMENTS.length) % QA_ELEMENTS.length]);
     let previousAnswer = document.querySelector(
           QA_ELEMENTS[(qaIndex - 1 + QA_ELEMENTS.length) % QA_ELEMENTS.length]);
+    let currentRow = document.querySelector(QA_ROWS[qaIndex / 2]);
+    let previousRow = document.querySelector(
+          QA_ROWS[((qaIndex / 2) - 1 + QA_ROWS.length) % QA_ROWS.length]);
+    let currentLink = document.querySelector(QA_LINKS[qaIndex / 2]);
+    let previousLink = document.querySelector(
+          QA_LINKS[((qaIndex / 2) - 1 + QA_LINKS.length) % QA_LINKS.length]);
 
     currentQuestion.setAttribute('class', 'text-end text-body-emphasis');
     previousQuestion.setAttribute('class', 'text-end text-body-tertiary');
     previousAnswer.setAttribute('class', 'text-start text-body-tertiary');
+    currentRow.setAttribute('class', 'table-active');
+    previousRow.setAttribute('class', '');
+    currentLink.setAttribute('class', 'link-primary');
+    previousLink.setAttribute('class', 'link-body-emphasis link-opacity-50');
   }
   else {
     let currentQuestion = document.querySelector(QA_ELEMENTS[qaIndex - 1]);
